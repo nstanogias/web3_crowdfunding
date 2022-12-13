7504f140260092123b04598f4aecce129c5dd496
nstanogias <nstanogias@gmail.com>
2022-12-13 23:11:24 +0100

    create pages

---
+++ client/pages/campain-details/[id].tsx
@@ -0,0 +1,5 @@
+const CampainDetails = () => {
+  return <div></div>;
+};
+
+export default CampainDetails;
---
+++ client/pages/create-campaign.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next';
+import React from 'react';
+
+const CreateCampaign: NextPage = () => {
+  return <div>create-campain</div>;
+};
+
+export default CreateCampaign;
