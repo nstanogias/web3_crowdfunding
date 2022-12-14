d5e87149536711d0140b0c86134ac49ae29fd8ca
nstanogias <nstanogias@gmail.com>
2022-12-14 21:56:32 +0100

    added navbar and sidenav

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    disableStaticImages: true,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
