b23f234d1ed1630fdee9901d96eb25820a162d75
nstanogias <nstanogias@gmail.com>
2023-01-25 11:54:34 +0100

    created profile page

--- client/pages/profile.tsx
+++ client/pages/profile.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import DisplayCampaigns from '../components/DisplayCampaigns';
+import { useApiContext } from '../context';
+import { ICampaign } from '../types';
 
-const Profile = () => {
-  return <div>profile</div>;
+const Profile: NextPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState<ICampaign[]>([]);
+
+  const { address, contract, getUserCampaigns } = useApiContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <DisplayCampaigns title='All Campaigns' isLoading={isLoading} campaigns={campaigns} />
+    </div>
+  );
 };
 
 export default Profile;
